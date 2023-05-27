953e542281c25946e69685b42ce5875fe56db278
khraina <84083826+khraina@users.noreply.github.com>
2023-05-27 22:03:36 +0530

    organizer side complete

--- static/js/new.js
+++ static/js/new.js
@@ -1,10 +1,9 @@
 
 const rateSpan = document.querySelector("#rateSpan")
 const rateForm = document.querySelector("#rateForm")
-//console.log(rateSpan)
 const baseUrl = "http://apilayer.net/api"
-const apiKey = "28851dac7733a3d2f0409f37f95ac9c9" // Add your API Key here
-let data; // undefined
+const apiKey = "28851dac7733a3d2f0409f37f95ac9c9" 
+let data; 
 
 
 
@@ -21,32 +20,15 @@ let data; // undefined
      let rate= data.carrier
 
      rateSpan.innerHTML= `<strong>${rate}<strong>`
-    // let currencyData = data.data
-    //console.log(currencyData)
-
-     //currencies = currencies.split(",")
     
-     //let htmlArray = currencies.map(currency => {
-        //return `<li>${currencyData[currency].code} - ${currencyData[currency].value}</li>`
-     //})
-    
-     //console.log(htmlArray)
-
-     //rateSpan.innerHTML = htmlArray.join("")
-
-     //console.log("Glad I could help you out")
  }
 
 
-// getNumber()
+
  rateForm.addEventListener("submit", handleSubmit)
 
   function handleSubmit(event){
       event.preventDefault()
       let pno= event.target["number"].value
-      getNumber(pno);
-      // let pno = rateForm.elements["number"].value
-      //   console.log("Hello world")
-      // getExchangeRate(pno);
-
- }
+      getNumber(pno)
+    }
